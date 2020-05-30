eceeab6298d5eb040a64771b9d80bdd2630b6f0b
ysku <yusuke1581@gmail.com>
2020-05-30 20:35:57 +0900

    init iac subpackage

---
+++ packages/iac/lib/iac-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2'
+
+export class IacStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
