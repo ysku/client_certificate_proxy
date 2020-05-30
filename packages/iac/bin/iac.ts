dbf0dbac91640978727447aa13f74567d3db6643
ysku <yusuke1581@gmail.com>
2020-05-30 20:44:49 +0900

    configure formatter

--- packages/iac/bin/iac.ts
+++ packages/iac/bin/iac.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { IacStack } from '../lib/iac-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { IacStack } from '../lib/iac-stack'
 
-const app = new cdk.App();
-new IacStack(app, 'IacStack');
+const app = new cdk.App()
+new IacStack(app, 'IacStack')
